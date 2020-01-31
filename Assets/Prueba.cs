869d0c974586e03cf98ab7b765a29cb65f06a9db
AlvaroRoger <49962993+AlvaroRoger@users.noreply.github.com>
2020-01-31 18:50:08 +0100

    Prueba

---
+++ Assets/Prueba.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+}
